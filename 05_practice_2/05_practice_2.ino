5124f7f9bc300df8545551f3f579fd83e1ebe258
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2021-10-10 20:11:34 +0900

    Update 05_practice_2.ino
    수정

--- 05_practice_2/05_practice_2.ino
+++ 05_practice_2/05_practice_2.ino
@@ -3,23 +3,26 @@ unsigned int toggle, toggle_update;
 
 void setup(){
   pinMode(PIN_LED, OUTPUT);
-  toggle = 0;
-  digitalWrite(PIN_LED, toggle); //turn on LED.
-  delay(1000); //wait for 1,000 milliseconds
-  
   toggle = 1;
-  while(toggle <= 10){
+}
+
+void loop(){
+  digitalWrite(PIN_LED, 0); //turn on LED.
+  delay(1000); //wait for 1,000 milliseconds
+
+  toggle = 1;
+  while(toggle <= 10) {
     toggle_update = toggle_state(toggle); //toggle LED value.
     digitalWrite(PIN_LED, toggle_update); //update LED status.
-    delay(200); //wait for 200 milliseconds
+    delay(100); //wait for 100 milliseconds
     toggle++;
-  }
+  }  
+  
+  digitalWrite(PIN_LED, 1); //turn off LED.
+  while(1){}//infinite loop
 }
 
 int toggle_state(int toggle){
   return toggle%2;
 }
 
-void loop(){
-   digitalWrite(PIN_LED, 1); //turn off LED.
-}
